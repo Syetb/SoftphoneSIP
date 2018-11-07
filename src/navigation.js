8a22f2529af8a834ad50bee22fdcfa17707e62cb
Jhoisaac <jhoisaac1191@gmail.com>
2018-11-06 19:33:47 -0500

    Proof of Concept - navigation using tabs with stack

--- src/navigation.js
+++ src/navigation.js
@@ -34,7 +34,7 @@ export const goToAuth = () => Navigation.setRoot({
     }
 });
 
-export const goHome = () => Navigation.setRoot({
+export const goHomez = () => Navigation.setRoot({
     root:{
         bottomTabs: {
             id: 'AppBottomTabsId',
@@ -87,6 +87,72 @@ export const goHome = () => Navigation.setRoot({
                         }
                     }
                 }
+            ]
+        }
+    }
+});
+
+export const goHome = () => Navigation.setRoot({
+    root: {
+        bottomTabs: {
+            children: [
+                {
+                    stack: {
+                        children: [
+                            {
+                                component: {
+                                    name: 'DialUp',
+                                    passProps: {
+                                        text: 'This is tab 1'
+                                    }
+                                }
+                            }
+                        ],
+                        options: {
+                            bottomTab: {
+                                text: 'Tab 1',
+                                icon: require('./assets/images/dialup.png'),
+                                testID: 'FIRST_TAB_BAR_BUTTON'
+                            }
+                        }
+                    }
+                },
+                {
+                    component: {
+                        name: 'Contacts',
+                        passProps: {
+                            text: 'This is tab 2'
+                        },
+                        options: {
+                            bottomTab: {
+                                text: 'Tab 2',
+                                icon: require('./assets/images/contacts.png'),
+                                testID: 'SECOND_TAB_BAR_BUTTON'
+                            }
+                        }
+                    }
+                },
+                {
+                    stack: {
+                        children: [
+                            {
+                                component: {
+                                    name: 'Profile',
+                                    passProps: {
+                                        text: 'This is tab 3'
+                                    }
+                                }
+                            }
+                        ],
+                        options: {
+                            bottomTab: {
+                                text: 'Tab 3',
+                                icon: require('./assets/images/profile.png'),
+                                testID: 'THIRD_TAB_BAR_BUTTON'
+                            }
+                        }
+                    }
+                },
             ]
         }
     }
