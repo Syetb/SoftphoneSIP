d5fe1e87168340ebf32023864ef25976e01074f1
Jhoisaac <jhoisaac1191@gmail.com>
2018-11-30 23:46:19 -0500

    Primera conexion websocket exitosa y registrado en asterisk

--- src/views/Screen2.js
+++ src/views/Screen2.js
@@ -7,6 +7,8 @@ import {
 } from 'react-native'
 import {Navigation} from 'react-native-navigation'
 
+import JsSIP from 'jssip'
+
 export default class Screen2 extends React.Component {
     static get options() {
         return {
@@ -17,12 +19,30 @@ export default class Screen2 extends React.Component {
             }
         };
     }
+
+    registerWebRTCPeer() {
+        // Create our JsSIP instance and run it:
+        const socket = new JsSIP.WebSocketInterface('wss://192.168.1.133:8089/ws');     // poner aviso si el servidor ws no es alcanzable
+        const configuration = {
+            sockets  : [ socket ],
+            uri      : 'sip:webrtc_client@192.168.1.133',
+            password : '5678'
+        };
+
+        const ua = new JsSIP.UA(configuration);
+
+        ua.start();
+        ua.register();
+
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <Text>Screen 2</Text>
                 <Button
-                    onPress={() => Navigation.pop(this.props.componentId)}
+                    // onPress={() => Navigation.pop(this.props.componentId)}
+                    onPress={() => this.registerWebRTCPeer()}
                     title="Go Back"
                 />
             </View>
