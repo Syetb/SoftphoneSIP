1935faef0c354138f4144ace63f45cfd039684fa
Jhoisaac <jhoisaac1191@gmail.com>
2019-01-24 23:15:11 -0500

    Implementacion action creator init() para initialize module pjsip

---
+++ src/actions/app.js
@@ -0,0 +1,61 @@
+import { Platform } from 'react-native'
+import { Endpoint } from 'react-native-pjsip'
+
+import { onAccountChanged, onConnectivityChanged, onCallReceived, onCallChanged, onCallTerminated, onCallScreenLocked } from './handlers'
+
+export const INIT = 'pjsip/INIT'
+export const CHANGED_APP_STATE = 'pjsip/CHANGED_APP_STATE'
+
+/**
+ * Initialize PjSIP functionality.
+ * First of all you have to initialize module to be able to work with it.
+ */
+export function init() {
+    return async function (dispatch, getState) {
+        // Retrieving PjSip service state
+        // It is possible that Endpoint instance already have registered accounts and active calls.
+        // (because Javascript state is not persistent when User close application, e.g. application goes to background state)
+        // It works in background because in Android where is a service PjSip service, that you included in AndroidManifest.xml.
+
+        const endpoint = new Endpoint()
+        const state = await endpoint.start({
+            service: {
+                ua: Platform.select({ios: "RnSIP iOS", android: "RnSIP Android"})
+            },
+            network: {
+                useWifi: true,
+                useOtherNetworks: true
+            }
+        })
+
+        const { accounts, calls, settings: endpointSettings, connectivity } = state
+
+        // Subscribe to endpoint events
+        endpoint.on("registration_changed", (account) => {
+            dispatch(onAccountChanged(account))
+        })
+        endpoint.on("connectivity_changed", (available) => {
+            dispatch(onConnectivityChanged(available))
+        })
+        endpoint.on("call_received", (call) => {
+            dispatch(onCallReceived(call))
+        })
+        endpoint.on("call_changed", (call) => {
+            dispatch(onCallChanged(call))
+        })
+        endpoint.on("call_terminated", (call) => {
+            dispatch(onCallTerminated(call))
+        })
+        endpoint.on("call_screen_locked", (call) => {
+            dispatch(onCallScreenLocked(call))
+        })
+
+        const accountMap = accounts.reduce( (acc, cur) => acc[cur.getId()] = cur, {} )
+        const callsMap = calls.reduce( (acc, cur) => acc[cur.getId()] = cur, {} )
+
+        dispatch({
+            type: INIT,
+            payload: { endpoint, endpointSettings, connectivity, accounts: accountMap, calls: callsMap }
+        })
+    }
+}
