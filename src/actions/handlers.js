196d6359f752c2beeaf2ae2b2e6696b39333298e
Jhoisaac <jhoisaac1191@gmail.com>
2019-01-24 23:16:31 -0500

    Implementacion handlers action creators para manejo de pjsip endpoint events

---
+++ src/actions/handlers.js
@@ -0,0 +1,77 @@
+export const ACCOUNT_CHANGED = 'pjsip/ACCOUNT_CHANGED'
+export const CONNECTIVITY_CHANGED = 'pjsip/CONNECTIVITY_CHANGED'
+
+export const CALL_RECEIVED = 'pjsip/CALL_RECEIVED'
+export const CALL_CHANGED = 'pjsip/CALL_CHANGED'
+export const CALL_TERMINATED = 'pjsip/CALL_TERMINATED'
+export const CALL_SCREEN_LOCKED = 'pjsip/CALL_SCREEN_LOCKED'
+
+/**
+ * Handle account change event.
+ *
+ * @param {Account} account
+ */
+export function onAccountChanged(account) {
+    return { type: ACCOUNT_CHANGED, payload: { account } }
+}
+
+/**
+ * Handle connectivity change event.
+ *
+ * @param online
+ */
+export function onConnectivityChanged(online) {
+    return async function (dispatch, getState) {
+        dispatch( { type: CONNECTIVITY_CHANGED, payload: online } )
+    }
+}
+
+/**
+ * Handles incoming call event.
+ *
+ * @param {Call} call
+ * @returns {Function}
+ */
+export function onCallReceived(call) {
+    return async function (dispatch, getState) {
+        dispatch( { type: CALL_RECEIVED, call } )
+    }
+}
+
+/**
+ * Handles call change event.
+ *
+ * @param {Call} call
+ * @returns {Function}
+ */
+export function onCallChanged(call) {
+    return async function (dispatch, getState) {
+        dispatch( { type: CALL_CHANGED, call } )
+    }
+}
+
+/**
+ * Handles call terminated event.
+ *
+ * @param {Call} call
+ * @returns {Function}
+ */
+export function onCallTerminated(call) {
+    return async function (dispatch, getState) {
+        dispatch( { type: CALL_CHANGED, call } )
+        dispatch( { type: CALL_TERMINATED, call } )
+    }
+}
+
+/**
+ * Handles screen lock event.
+ * Android only
+ *
+ * @param bool lock
+ * @returns {Function}
+ */
+export function onCallScreenLocked(lock) {
+    return async function (dispatch, getState) {
+        dispatch ( { type: CALL_SCREEN_LOCKED, lock } )
+    }
+}
