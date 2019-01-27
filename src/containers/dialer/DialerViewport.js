ea2214b6a6d0c5cfda9694e34ca20338af39ec8c
Jhoisaac <jhoisaac1191@gmail.com>
2019-01-27 00:57:47 -0500

    Fixed missing PropTypes import

--- src/containers/dialer/DialerViewport.js
+++ src/containers/dialer/DialerViewport.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
 import KeypadWithActions from '../../components/call/KeypadWithActions'
+import { makeCall } from "../../actions/pjsip";
+
 import sc from '../../assets/styles/containers'
 
 const DialerViewport = ({ onCallPress }) => {
@@ -16,7 +19,7 @@ const DialerViewport = ({ onCallPress }) => {
 }
 
 DialerViewport.propTypes = {
-
+    onCallPress: PropTypes.func.isRequired
 }
 
 const mapStateToProps = (state) => {
@@ -24,7 +27,13 @@ const mapStateToProps = (state) => {
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return {}
+    return {
+        onCallPress: (destination) => {
+            if (destination) {
+                dispatch(makeCall(destination))
+            }
+        }
+    }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(DialerViewport)
