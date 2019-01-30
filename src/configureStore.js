6f2b11e9e766733dfbc85da7322a98ecda0f844a
Jhoisaac <jhoisaac1191@gmail.com>
2019-01-30 14:58:07 -0500

    Se agrega react-native-debugger al store de redux - para depuracion de la app

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 import thunk  from 'redux-thunk';
 
@@ -9,7 +9,16 @@ import thunk  from 'redux-thunk';
 //     return store
 // }
 
-export default function configureStore(initialState) {
-    const finalCreateStore = applyMiddleware(thunk)(createStore);
-    return finalCreateStore(rootReducer, initialState)
+// export default function configureStore(initialState) {
+//     const finalCreateStore = applyMiddleware(thunk)(createStore);
+//     return finalCreateStore(rootReducer, initialState)
+// }
+
+export default function configureStore() {
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
+        applyMiddleware(thunk)
+    ));
+
+    return store
 }
