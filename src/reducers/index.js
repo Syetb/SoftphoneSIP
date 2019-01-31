73d9320b16f87adf2ef08f591458793dd6c5fe2b
Jhoisaac <jhoisaac1191@gmail.com>
2019-01-31 13:53:57 -0500

    Se agrega el reducer navigate al rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import pjsipReducer from './pjsip.reducer';
+import navigation from './navigate.reducer'
 
 const rootReducer = combineReducers({
-    pjsip: pjsipReducer
+    pjsip: pjsipReducer,
+    navigate: navigation
 });
 
 export default rootReducer;
