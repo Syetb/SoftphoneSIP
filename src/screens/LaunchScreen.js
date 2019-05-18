2bd5404db002bf3b1ff54abf5a4db297795bd5c7
Jhoisaac <jhoisaac1191@gmail.com>
2019-05-18 18:16:35 -0500

    Se cambia idioma interfaz

--- src/screens/LaunchScreen.js
+++ src/screens/LaunchScreen.js
@@ -20,7 +20,7 @@ export default class Initialising extends Component {
         return (
             <View style={styles.container}>
                 <Text style={styles.welcome}>
-                    Loading...
+                    Iniciando...
                 </Text>
             </View>
         )
