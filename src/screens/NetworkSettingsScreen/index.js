e94ea97536af7e9c0eea030b58fc8d407adc3c8b
Jhoisaac <jhoisaac1191@gmail.com>
2019-05-18 18:43:07 -0500

    Red utilizados en solo lectura final

--- src/screens/NetworkSettingsScreen/index.js
+++ src/screens/NetworkSettingsScreen/index.js
@@ -139,15 +139,15 @@ class NetworkSettingsScreen extends Component {
                 <Header title={"Ajustes de Red"} {...platformHeaderProps} />
 
                 <ScrollView style={cs.mainContainer}>
-                    <ListSection title="Configuraciones de Conectividad"/>
+                    /*<ListSection title="Configuraciones de Conectividad"/>
 
                     <ListCheckbox
                         disabled={this.state.wifiDisabled}
                         onChange={this._onWifiChange}
-                        value={true} /*this.state.useWifi*/
+                        value={this.state.useWifi}
                         title="Usar WiFi"
                         description="Usa WiFi para llamadas entrantes y salientes"
-                    />
+                    />*/
 
                     <ListSection title="Conexion en Background"/>
 
