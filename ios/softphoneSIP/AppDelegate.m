72acfc1ace974a3139801568564e42b6a4c4ed65
Jhoisaac <jhoisaac1191@gmail.com>
2019-08-21 18:57:41 -0500

    Finished app SoftphoneSIP bundleJS - new directories

--- ios/softphoneSIP/AppDelegate.m
+++ ios/softphoneSIP/AppDelegate.m
@@ -11,7 +11,7 @@
 {
   NSURL *jsCodeLocation;
   
-  // jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+//   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
   
   jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
   
